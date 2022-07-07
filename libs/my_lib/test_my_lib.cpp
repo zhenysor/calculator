281fb72fb615a799965c80a47986238ac46693f0
jenys <Zheny.Sor@yandex.ru>
2022-07-07 19:32:46 +0300

    master: добавлен тест на вывод в поток

--- libs/my_lib/test_my_lib.cpp
+++ libs/my_lib/test_my_lib.cpp
@@ -4,3 +4,21 @@
 #include "assert.h"
 
 class ProjectTest : public ::testing::Test {};
+
+TEST(ProjectTest, classCalcAndPers_outoperator_nameAndSpending)
+{
+	Calculator::Pers kirill("kirill", 1000);
+	std::cout << kirill << std::endl;
+	Calculator::Pers ksenia("ksenia", 500);
+	std::cout << ksenia << std::endl;
+
+	std::cout << std::endl;
+
+	std::list<Calculator::Pers> list;
+	list.push_back(kirill);
+	list.push_back(ksenia);
+	list.push_back(kirill);
+	list.push_back(ksenia);
+
+	std::cout << list << std::endl;
+}
