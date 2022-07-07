3a2f5704594cf77c510d11c3baa40f6eafd9d8c4
jenys <Zheny.Sor@yandex.ru>
2022-07-07 19:32:01 +0300

    master: добавлен класс Calculator и Pers, поддержаны операторы вывода

--- libs/my_lib/my_lib.h
+++ libs/my_lib/my_lib.h
@@ -1,5 +1,52 @@
 #ifndef MY_LIB_H
 #define MY_LIB_H
 
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <list>
+//#include <math.h>
+
+class Calculator
+{
+public:
+
+	class Pers
+	{
+	public:
+
+		Pers(std::string n, int s) : name(n), spending(s)
+		{}
+
+		std::string name;
+		int spending;
+	};
+
+	size_t personCount() const
+	{
+		return company.size();
+	}
+
+	std::list<Pers> company;
+};
+
+std::ostream& operator<<(std::ostream &out, const Calculator::Pers &pers)
+{
+	out << pers.name;
+	out << " - ";
+	out << pers.spending;
+	return out;
+}
+
+std::ostream& operator<<(std::ostream &out, const std::list<Calculator::Pers> &comp)
+{
+	for(const Calculator::Pers &pers : comp) {
+		out << pers;
+		out << std::endl;
+	}
+	return out;
+}
+
+//std::ifstream
 
 #endif // MY_LIB_H
